cbcefb35e36f4dbc982815815bb432c27c9b3c74
aammddkkzxc <ideal0427@naver.com>
2024-02-05 00:53:20 +0900

    quiz

--- _0204weeklyQuiz/Main.java
+++ _0204weeklyQuiz/Main.java
@@ -1,10 +1,24 @@
 package _0204weeklyQuiz;
 
 import _0204weeklyQuiz.ProductType.Beauty;
+import _0204weeklyQuiz.ProductType.Grocery;
+import _0204weeklyQuiz.ProductType.LargeAppliance;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Beauty beauty = new Beauty("마스카라", 100, 10000, true);
-        System.out.println(beauty.getDeliveryCharge());
+        Product beauty = new Beauty("마스카라", 10, 10000, true);
+        Product grocery = new Grocery("깻잎", 1, 3000, 1);
+        Product largeAppliance = new LargeAppliance("냉장고", 500, 1000000, 900);
+
+        List<Product> products = new ArrayList<>();
+        products.add(beauty);
+        products.add(grocery);
+        products.add(largeAppliance);
+
+        for(Product product : products) {
+            System.out.println(product.getDeliveryCharge());
+        }
     }
 }
