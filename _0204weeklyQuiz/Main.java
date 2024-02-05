889c8fc7f41120e98f22fe8a6babb7ab6394be79
aammddkkzxc <ideal0427@naver.com>
2024-02-05 17:14:04 +0900

    fix: 오류 수정

--- _0204weeklyQuiz/Main.java
+++ _0204weeklyQuiz/Main.java
@@ -6,18 +6,17 @@ import _0204weeklyQuiz.ProductType.LargeAppliance;
 
 public class Main {
     public static void main(String[] args) {
-        Product beauty = new Beauty("beauty", 30000, 2);
-        Product grocery = new Grocery("grocery", 20000, 3);
-        Product largeAppliance = new LargeAppliance("largeAppliance", 50000, 5);
+        Product beauty = new Beauty("beauty", 2, 30000);
+        Product grocery = new Grocery("grocery", 3, 20000);
+        Product largeAppliance = new LargeAppliance("largeAppliance", 5, 50000);
 
-        int totalPromotion = beauty.getDiscountAmount() + grocery.getDiscountAmount() + largeAppliance.getDiscountAmount();
+//        int totalPromotion = beauty.getDiscountAmount() + grocery.getDiscountAmount() + largeAppliance.getDiscountAmount();
 
         Cart cart = new Cart(new Product[] {beauty, grocery, largeAppliance});
         int totalDeliveryCharge = 0;
         for(Product product : cart.getProducts()) {
             totalDeliveryCharge += cart.calculateDeliveryCharge(product);
         }
-
-        System.out.println(totalDeliveryCharge - totalPromotion);    // 결과: 9000
+        System.out.println(totalDeliveryCharge);    // 결과: 9000
     }
 }
