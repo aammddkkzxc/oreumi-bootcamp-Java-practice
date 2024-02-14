90d66bd849e838ce2d07381cd1002047e0fe2534
aammddkkzxc <ideal0427@naver.com>
2024-02-14 10:35:39 +0900

    feat: 번호 정규식 검사 기능 구현

--- _0213_Address_Program/Contact.java
+++ _0213_Address_Program/Contact.java
@@ -1,15 +1,24 @@
 package _0213_Address_Program;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Contact {
     private final String name;
     private final String phoneNumber;
 
     public Contact(String name, String phoneNumber) {
+        checkRegex(phoneNumber);
         //각종 유효성 검사 로직 구현
         this.name = name;
         this.phoneNumber = phoneNumber;
+    }
+
+    private void checkRegex(String phoneNumber) {
+        Pattern pattern = Pattern.compile("\\d{3}-\\d{4}-\\d{4}");
+        if(!pattern.matcher(phoneNumber).find()) {
+            throw new IllegalArgumentException("번호 형식에 맞게 다시 입력해 주세요");
+        }
     }
 
     public boolean isNumberDuplicate(Contact other) {
